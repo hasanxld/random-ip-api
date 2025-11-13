847eff040fe18caa9eeda838960befd31a524876
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 12:16:53 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+      animation: {
+        'gradient': 'gradient 8s linear infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      }
+    },
+  },
+  plugins: [],
+}
