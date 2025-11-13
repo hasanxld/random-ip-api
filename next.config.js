cae31c024c2f06c1ec71a2931add49fa8ea26a1c
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-13 13:34:22 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // For Netlify deployment
+  // For Netlify
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  // Output standalone for better performance
-  output: 'standalone',
 }
 
 module.exports = nextConfig
